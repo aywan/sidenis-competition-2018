328ad2be675895b07c5bc170b9a0362d04fb5b1e
Ivan Bragin <aywan@aywan.ru>
2018-11-26 11:12:51 +0700

    17s, Reader class

--- src/ru/aywan/sidenis/Main.java
+++ src/ru/aywan/sidenis/Main.java
@@ -1,67 +1,53 @@
 package ru.aywan.sidenis;
 
-import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        BufferedReader bi = new BufferedReader(new InputStreamReader(System.in));
-        String[] list;
-        int size = Integer.parseInt(bi.readLine());
-        MatrixSummator s = new MatrixSummator(size, bi);
-        int opCount = Integer.parseInt(bi.readLine());
+        Reader r = new Reader();
+        int size = r.nextInt();
+        MatrixSummator s = new MatrixSummator(size, r);
+        int opCount = r.nextInt();
         for (int k = 0; k < opCount; k++) {
-            list = bi.readLine().split(" ");
-            s.parseOp(list);
+            s.readOp(r);
         }
+        r.close();
     }
 }
 
 class MatrixSummator {
-    private static final String MOD = "2";
-    private static final String SUM = "1";
+    private static final int MOD = 2;
+    private static final int SUM = 1;
 
     private final int size;
     private final long[][] mtSum;
     private final long[][] mt;
 
-    MatrixSummator(int size, BufferedReader r) throws IOException {
+    MatrixSummator(int size, Reader r) throws IOException {
         this.size = size + 1;
         this.mtSum = new long[this.size][this.size];
         this.mt = new long[this.size][this.size];
 
-        String[] list;
-
         for (int i = 1; i < this.size; i++) {
-            list = r.readLine().split(" ");
             for (int j = 1; j < this.size; j++) {
-                this.mt[j][i] = Long.parseLong(list[j - 1]);
+                this.mt[j][i] = r.nextLong();
                 this.mtSum[j][i] = this.mt[j][i] + this.mtSum[j][i - 1];
             }
         }
     }
 
-    void parseOp(String[] args) {
-        switch (args[0]) {
+    void readOp(Reader r) throws IOException {
+        int op = r.nextInt();
+
+        switch (op) {
             case MOD:
-                this.modValue(
-                        Integer.parseInt(args[1]),
-                        Integer.parseInt(args[2]),
-                        Long.parseLong(args[3])
-                );
+                this.modValue(r.nextInt(), r.nextInt(), r.nextLong());
                 break;
 
             case SUM:
-                this.printSum(
-                        Integer.parseInt(args[1]),
-                        Integer.parseInt(args[2]),
-                        Integer.parseInt(args[3]),
-                        Integer.parseInt(args[4])
-                );
+                this.printSum(r.nextInt(), r.nextInt(), r.nextInt(), r.nextInt());
                 break;
         }
     }
@@ -93,5 +79,83 @@ class MatrixSummator {
         for (int i = y; i < this.size; i++) {
             this.mtSum[x][i] += diff;
         }
+    }
+}
+
+class Reader {
+    final private int BUFFER_SIZE = 1 << 16;
+    private DataInputStream din;
+    private byte[] buffer;
+    private int bufferPointer, bytesRead;
+
+    Reader() {
+        this.din = new DataInputStream(System.in);
+        this.buffer = new byte[this.BUFFER_SIZE];
+        this.bufferPointer = 0;
+        this.bytesRead = 0;
+    }
+
+    int nextInt() throws IOException {
+        int ret = 0;
+        byte c = this.read();
+
+        while (c <= ' ') {
+            c = this.read();
+        }
+
+        boolean neg = (c == '-');
+        if (neg) {
+            c = this.read();
+        }
+
+        do {
+            ret = ret * 10 + c - '0';
+            c = this.read();
+        } while (c >= '0' && c <= '9');
+
+        return neg ? -ret : ret;
+    }
+
+    long nextLong() throws IOException {
+        long ret = 0;
+        byte c = this.read();
+
+        while (c <= ' ') {
+            c = this.read();
+        }
+
+        boolean neg = (c == '-');
+        if (neg) {
+            c = this.read();
+        }
+
+        do {
+            ret = ret * 10 + c - '0';
+            c = this.read();
+        } while (c >= '0' && c <= '9');
+
+        return neg ? -ret : ret;
+    }
+
+    private void fillBuffer() throws IOException {
+        this.bufferPointer = 0;
+        this.bytesRead = this.din.read(this.buffer, 0, this.BUFFER_SIZE);
+        if (this.bytesRead == -1) {
+            this.buffer[0] = -1;
+        }
+    }
+
+    private byte read() throws IOException {
+        if (this.bufferPointer == this.bytesRead) {
+            this.fillBuffer();
+        }
+        return this.buffer[this.bufferPointer++];
+    }
+
+    void close() throws IOException {
+        if (this.din == null) {
+            return;
+        }
+        this.din.close();
     }
 }
